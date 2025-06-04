33af6db4e41f70a99f31dd577909f23d2710bba4
Jens <jnshsrs@users.noreply.github.com>
2025-06-04 22:08:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "genai" {
   name     = "rg-genai-dev"
   location = "westeurope"
 }
+
+module "postgresql" {
+  source              = "./modules/postgresql"
+  environment         = var.environment
+  location            = var.location
+  resource_group_name = azurerm_resource_group.genai.name
+  admin_password      = var.pg_admin_password
+}
