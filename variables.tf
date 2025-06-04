4d98c9c9db5833c13847d7cd93846338cc4a5e53
Jens <jnshsrs@users.noreply.github.com>
2025-06-04 22:03:22 +0200

    Update variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  description = "Azure location"
+  type        = string
+  default     = "westeurope"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
